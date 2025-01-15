141de93b9952127d6e0bb841c0d5234bae200acf
ankitsahu74 <sahu74824@gmail.com>
2025-01-15 19:33:46 +0530

    Your commit message here

---
+++ Addition.java
@@ -0,0 +1,12 @@
+package corebasic;
+
+public class Addition {
+	public static void main(String[]args) {
+		int a=Integer.parseInt(args[0]);
+		int b=Integer.parseInt(args[1]);
+		int sum=a+b;
+		System.out.println("sum of"+a+"and"+b+"="+sum);
+		
+	}
+
+}
